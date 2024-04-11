f746c0c9627a58d8611e750347092378f3f5d375
Jeriah Marson <jeriahmarson@gmail.com>
2024-04-11 09:05:03 -0700

    react-ntative

---
+++ Screens/LoginScreen.tsx
@@ -0,0 +1,26 @@
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import LoginFormComponent from '../Components/LoginFormComponent'
+
+const LoginScreen = () => {
+  return (
+    <ImageBackground source={require('../assets/pickture.jpg')} style={styles.backGround}>
+
+        <View>
+            <LoginFormComponent/>
+        </View>
+
+    </ImageBackground>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    backGround: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+})
